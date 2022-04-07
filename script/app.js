d2e914920c2e1f2c3c9a1086a743ad3ead8602b1
Mubarek-Hassen <Mubarekhassen4@gmail.com>
2022-04-07 15:52:18 -0400

    Time for button colorsset.

--- script/app.js
+++ script/app.js
@@ -26,9 +26,9 @@ btnYellow.addEventListener('click',()=>{
 
 btnBlue.addEventListener('click', ()=>{
     console.log('blue is pressed')
-    screen.classList.add('blue')
+    screen.classList.add(simon[2])
     setTimeout(()=>{
-        screen.classList.remove('blue')
+        screen.classList.remove(simon[2])
     },500)
 })
 btnGreen.addEventListener('click', ()=>{
@@ -54,3 +54,46 @@ start.addEventListener('click', ()=>{
     start.innerHTML = 'Reset'
     
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// 1. first, build a playground for the player and Simon.
+    // a. create a div that holds all the elemnts to the game.
+    // b. create/append elements to the browser to create the playground
+
+// 2. On the playground, we create a screen for simon to pass his order of colors. Simon will be calling out from 4 different colors.
+    // a. 
+
+// 3. on the player's side of the playground, there are 4 buttons that are equivalent with the colors from Simon.
+// 4. Simon will be assigned an array of colors and he will randomly print the colors within the setInterval.
+// 5. the player's buttons are also assigned an array of colors that match Simon's colors array.
+// 6. if the player's clicked button match Simon's printed color, score will increment. else, score will decrement and player gets to lose a chance to fail again. If player fails twice, game will be over and score will show up on simon's screen.
+// 7. if player keeps on clicking the right pattern, speed/ time interval of simon printing the colors will increase, making it more difficult each round to follow along Simon.
